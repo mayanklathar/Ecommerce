43a43c3b6bf92c7a00cac351b2e55a18e210e4c8
mayanklathar <31530576+mayanklathar@users.noreply.github.com>
2017-12-04 17:19:26 +0530

    linking backend with ui

---
+++ projectbackend/src/main/java/com/projectbackend/dao/CategoryDAO.java
@@ -0,0 +1,13 @@
+package com.projectbackend.dao;
+
+import java.util.List;
+
+import com.projectbackend.dto.Category;
+
+public interface CategoryDAO {
+
+
+
+	List<Category> list();
+
+}
---
+++ projectbackend/src/main/java/com/projectbackend/daoImpl/CategoryDAOImpl.java
@@ -0,0 +1,31 @@
+package com.projectbackend.daoImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.projectbackend.dao.CategoryDAO;
+import com.projectbackend.dto.Category;
+
+public class CategoryDAOImpl implements CategoryDAO {
+
+	private static List<Category> categories= new  ArrayList<Category>();
+	
+	static{
+Category category = null;
+		//adding first category
+		category.setId(1);
+		category.setName("televison");
+		category.setDescription("description of tv");
+		category.setImageURL("CAT_1.png");
+		
+		category.setId(2);
+		category.setName("phone ");
+		category.setDescription("description of phone");
+		category.setImageURL("CAT_2.png");
+	}
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return categories;
+	}
+	
+}
